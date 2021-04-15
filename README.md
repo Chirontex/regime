8e0fa3ad551004da1e064989d6c8519ba9a27ceb
Dmitry Shumilin <infernozzz@yandex.ru>
2021-04-15 23:20:16 +0300

    0.1.3: update form edit view

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Regime 0.1.2
+# Regime 0.1.3
--- admin/form-edit.php
+++ admin/form-edit.php
@@ -14,12 +14,73 @@ else esc_html_e('Редактирование формы', 'regime');
 
 ?>
     </h1>
-    <div>
-        <label for="regimeFormType" class="form-label"><?= esc_html__('Тип формы:', 'regime') ?></label>
-        <select name="regimeFormType" id="regimeFormType" class="form-select form-select-sm" style="max-width: 200px;">
-            <option value="registration"<?= $_GET['ftype'] === 'registration' ? 'selected' : '' ?>><?= esc_html__('Регистрация', 'regime') ?></option>
-            <option value="authorization"<?= $_GET['ftype'] === 'authorization' ? 'selected' : '' ?>><?= esc_html__('Авторизация', 'regime') ?></option>
-            <option value="profile"<?= $_GET['ftype'] === 'profile' ? 'selected' : '' ?>><?= esc_html__('Профиль', 'regime') ?></option>
-        </select>
+    <div class="mb-5 mx-auto" style="max-width: 500px;">
+        <div class="text-center mb-3">
+            <?= esc_html__('Тип формы:', 'regime') ?>
+        </div>
+        <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+                <input type="radio" name="regimeFormType" id="regimeFormTypeRegistration" value="registration"<?= $_GET['ftype'] === 'registration' ? 'checked' : '' ?>>
+                <label for="regimeFormTypeRegistration" class="form-label"><?= esc_html__('Регистрация', 'regime') ?></label>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+                <input type="radio" name="regimeFormType" id="regimeFormTypeAuthorization" value="authorization"<?= $_GET['ftype'] === 'authorization' ? 'checked' : '' ?>>
+                <label for="regimeFormTypeAuthorization" class="form-label"><?= esc_html__('Авторизация', 'regime') ?></label>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+                <input type="radio" name="regimeFormType" id="regimeFormTypeProfile" value="profile"<?= $_GET['ftype'] === 'profile' ? 'checked' : '' ?>>
+                <label for="regimeFormTypeProfile" class="form-label"><?= esc_html__('Профиль', 'regime') ?></label>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-4">
+            <h4 class="text-center mb-5">
+                <?= esc_html__('Добавление полей', 'regime') ?>
+            </h4>
+            <div class="mb-3">
+                <div class="text-center mb-3">
+                    <?= esc_html__('Ввод:', 'regime') ?>
+                </div>
+                <div class="text-center">
+                    <button class="btn btn-sm btn-outline-primary mb-1"><?= esc_html__('Текст', 'regime') ?></button>
+                    <button class="btn btn-sm btn-outline-primary mb-1"><?= esc_html__('Число', 'regime') ?></button>
+                    <button class="btn btn-sm btn-outline-primary mb-1"><?= esc_html__('E-mail', 'regime') ?></button>
+                    <button class="btn btn-sm btn-outline-primary mb-1"><?= esc_html__('Пароль', 'regime') ?></button>
+                    <button class="btn btn-sm btn-outline-primary mb-1"><?= esc_html__('Телефон', 'regime') ?></button>
+                    <button class="btn btn-sm btn-outline-primary mb-1"><?= esc_html__('URL', 'regime') ?></button>
+                </div>
+            </div>
+            <div class="mb-3">
+                <div class="text-center mb-3">
+                    <?= esc_html__('Чекер:', 'regime') ?>
+                </div>
+                <div class="text-center">
+                    <button class="btn btn-sm btn-outline-success mb-1"><?= esc_html__('Чекбокс', 'regime') ?></button>
+                    <button class="btn btn-sm btn-outline-success mb-1"><?= esc_html__('Радио', 'regime') ?></button>
+                </div>
+            </div>
+            <div class="mb-3">
+                <div class="text-center mb-3">
+                    <?= esc_html__('Выбор:', 'regime') ?>
+                </div>
+                <div class="text-center">
+                    <button class="btn btn-sm btn-outline-dark mb-1"><?= esc_html__('Список', 'regime') ?></button>
+                    <button class="btn btn-sm btn-outline-dark mb-1"><?= esc_html__('Предиктивный', 'regime') ?></button>
+                </div>
+            </div>
+            <div class="mb-3">
+                <div class="text-center mb-3">
+                    <?= esc_html__('Прочее:', 'regime') ?>
+                </div>
+                <div class="text-center">
+                    <button class="btn btn-sm btn-outline-secondary mb-1"><?= esc_html__('Текстовое поле', 'regime') ?></button>
+                    <button class="btn btn-sm btn-outline-secondary mb-1"><?= esc_html__('Кнопка сброса', 'regime') ?></button>
+                </div>
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-8">
+        
+        </div>
     </div>
 </div>
